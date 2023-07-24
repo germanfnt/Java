d3c437aa71d405965b2f8e47b8935d1df40bb3b2
german <germanfntorres@gmail.com>
2023-07-24 19:09:28 -0300

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,48 @@
+public class Variables {
+
+    public static void main (String[] args) {
+
+
+
+        //Variables primitivos y referencia
+        /*
+            Definicion de variables camelCase
+
+            "variableEntera" "variableEnteraCompuesta"
+
+            los caracteres solo unicode
+
+            palabras reservadas por ejemplo true, false, for, etc.
+
+            variables son unicas en cuestion de nombre
+
+            pueden comenzar por una letra por subrayado _ o simbolo $
+
+         */
+
+        byte varByte = (byte) 128;
+        short varShort = 1000 ;
+        int varInt = 12 ;
+        long varLong  ;
+
+        double varDouble = 1.2 ;
+        float varFloat = 1.3f ;
+
+        char varChar ;
+
+        Integer integer = 12;
+
+        Double double ;
+
+
+
+        String varString = "Curso Java";
+
+        System.out.println(varString.toUpperCase());
+
+
+
+
+    }
+
+}
